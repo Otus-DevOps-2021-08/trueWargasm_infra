cfee1bc5c2eae2868dcc8f04f798d26f39737e27
Roman Shirokov <super@wargasm.ru>
2021-12-12 22:22:29 +0300

    Fix stage

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -4,16 +4,19 @@ provider "yandex" {
   folder_id                = var.folder_id
   zone                     = var.zone
 }
-module "app" {
-  source          = "../modules/app"
-  public_key_path = var.public_key_path
-  app_disk_image  = var.app_disk_image
-  subnet_id       = var.subnet_id
-}
-
 module "db" {
   source          = "../modules/db"
   public_key_path = var.public_key_path
+  privat_key_path = var.privat_key_path
   db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
+}
+
+module "app" {
+  source          = "../modules/app"
+  public_key_path = var.public_key_path
+  privat_key_path = var.privat_key_path
+  app_disk_image  = var.app_disk_image
+  database_url    = "${module.db.external_ip_address_db}"
   subnet_id       = var.subnet_id
 }
